ff435306769454d6d64c4e420b591329a73cae3d
yassine <bakiri.yassine@gmail.com>
2018-07-07 22:45:54 +0200

    [Update] : style Number tile

--- src/configFonts.js
+++ src/configFonts.js
@@ -3,7 +3,7 @@ const WebFont = require('webfontloader');
 export function LoadFonts() {
     const config = {
         google: {
-            families: ['Roboto:100, 300']
+            families: ['Lato:100, 300, 400']
         }
     };
     WebFont.load(config, 0.1, true);
