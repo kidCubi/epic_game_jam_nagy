822168b1fac46d02a55b14527f17dadb535381e8
yassine <bakiri.yassine@gmail.com>
2018-07-07 15:49:49 +0200

    [Add] : NumberTIle

--- src/components/NumberTile/NumberTile.js
+++ src/components/NumberTile/NumberTile.js
@@ -18,28 +18,58 @@ class NumberTile extends Component {
     constructor() {
         super();
         this.state = {
-            numberCount: 1
+            progress: 0,
+            multiplier: 3,
+            totalPoints: 0
         };
 
+        this.rafId = null;
+
         this.incrementTile = this.incrementTile.bind(this);
+        this.resetCount = this.resetCount.bind(this);
+        this.addPoints = this.addPoints.bind(this);
+        this.raf = this.raf.bind(this);
     }
 
     componentDidMount() {
-        console.log('NumberTile')
+
     }
 
     incrementTile() {
-        let count = this.state.numberCount;
-        count++;
+        let count = this.state.progress;
+        count += 20;
         this.setState(state => ({
-            numberCount: count
+            progress: count
         }));
+        if(this.state.progress >= 100) {
+            this.resetCount()
+        }
+    }
+
+    resetCount() {
+        this.setState(state => ({
+            progress: 0
+        }));
+        if(this.state.progress === 0) return;
+        this.addPoints();
+    }
+
+    addPoints() {
+        this.setState(state => ({
+            totalPoints: this.state.totalPoints + this.state.multiplier
+        }));
+    }
+
+    raf() {
+        this.rafId = requestAnimationFrame(this.raf);
     }
 
     render() {
         return (
             <div className={styles.Wrapper} onClick={this.incrementTile}>
-                <span>{this.state.numberCount}</span>
+                <span>{this.state.progress}%</span>
+                <span className={styles.Multiplier}>+{this.state.multiplier}</span>
+                <span className={styles.TotalPoints}>{this.state.totalPoints}</span>
             </div>
         );
     }
